c0caddc3c79c4cdb5563264b7aa0ea7dc0695831
Víctor Orozco <vorozco@nabenik.com>
2018-07-26 17:58:29 -0600

    Agregando base de persitencia

---
+++ src/main/java/com/nabenik/jmovies/model/Movie.java
@@ -0,0 +1,112 @@
+package com.nabenik.jmovies.model;
+
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Movie implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+	private static final long serialVersionUID = 1L;
+	@Version
+	@Column(name = "version")
+	private int version;
+
+	@Column
+	@NotNull
+	private String nombre;
+
+	@Column
+	@Size(max = 2000)
+	private String descripcion;
+
+	@Column
+	private String imdb;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Movie)) {
+			return false;
+		}
+		Movie other = (Movie) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getImdb() {
+		return imdb;
+	}
+
+	public void setImdb(String imdb) {
+		this.imdb = imdb;
+	}
+
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (nombre != null && !nombre.trim().isEmpty())
+			result += "nombre: " + nombre;
+		if (descripcion != null && !descripcion.trim().isEmpty())
+			result += ", descripcion: " + descripcion;
+		if (imdb != null && !imdb.trim().isEmpty())
+			result += ", imdb: " + imdb;
+		return result;
+	}
+}
