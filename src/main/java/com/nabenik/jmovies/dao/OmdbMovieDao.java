da2701e8a70ee6fe7e7c34909379fdb7ba1f5b3b
Víctor Orozco <vorozco@nabenik.com>
2018-07-27 01:40:20 -0600

    Agregando cliente reactivo JAX-RS

---
+++ src/main/java/com/nabenik/jmovies/dao/OmdbMovieDao.java
@@ -0,0 +1,40 @@
+package com.nabenik.jmovies.dao;
+
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.TimeUnit;
+import javax.ejb.Stateless;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author tuxtor
+ */
+@Stateless
+public class OmdbMovieDao {
+    
+    private static final String OMDB_KEY = "380e56c9";
+    private static final String OMDB_BASE_URL = "http://www.omdbapi.com/?apikey=" + OMDB_KEY;
+    
+    public CompletionStage<String> findActors(String imdb){
+        
+        String requestUrl = OMDB_BASE_URL + "&i=" + imdb;
+        
+        //Intentar buscar los detalles
+        //Parametrizamos el cliente
+        WebTarget target = ClientBuilder.newBuilder()
+                .connectTimeout(2, TimeUnit.SECONDS)
+                .readTimeout(2, TimeUnit.SECONDS)
+                .build()
+                .target(requestUrl);
+
+        CompletionStage<String> future = target.request()
+                .accept(MediaType.APPLICATION_JSON)
+                .rx()
+                .get(String.class);
+        return future;
+    }
+    
+    
+}
--- src/main/java/com/nabenik/jmovies/rest/MovieEndpoint.java
+++ src/main/java/com/nabenik/jmovies/rest/MovieEndpoint.java
@@ -21,10 +21,14 @@ import com.nabenik.jmovies.dao.MovieDao;
 import com.nabenik.jmovies.dao.OmdbMovieDao;
 import com.nabenik.jmovies.model.Movie;
 import java.io.StringReader;
+import java.util.concurrent.CompletionStage;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.bind.JsonbBuilder;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 
 @RequestScoped
 @Path("/movies")
@@ -34,6 +38,9 @@ public class MovieEndpoint {
 	
 	@EJB
 	MovieDao movieDao;
+        
+        @EJB
+        OmdbMovieDao omdbDao;
 	
 	@POST
 	public Response create(Movie movie) {
@@ -76,30 +83,34 @@ public class MovieEndpoint {
         
         @GET
 	@Path("/with-actors/{id:[0-9][0-9]*}")
-	public Response findWithActors(@PathParam("id") final Long id) {
+	public void findWithActors(@PathParam("id") final Long id, @Suspended AsyncResponse response) {
+            
 		Movie movie = movieDao.findById(id);
-		if (movie == null) {
-			return Response.status(Status.NOT_FOUND).build();
-		}
                 
-		return Response.ok(createMovieWithActor(movie)).build();
+		if (movie == null) {
+			response.resume(new NotFoundException());
+		}
+                String movieString = JsonbBuilder.create().toJson(movie);
+                
+                CompletionStage<String> omdbInfo = omdbDao.findActors(movie.getImdb());
+                
+                omdbInfo.thenApply((omdbResponse) -> {
+                    
+                    JsonReader orgMovieJsonReader = Json.createReader(new StringReader(movieString));
+                    JsonObject orgMovie = orgMovieJsonReader.readObject(); 
+                    
+                    JsonReader omdbJsonReader = Json.createReader(new StringReader(omdbResponse));
+                    JsonObject omdbMovie = omdbJsonReader.readObject(); 
+                    
+                    //Json-p Patch
+                    orgMovie = Json.createPatchBuilder()
+                      .add("/actores", omdbMovie.getString("Actors", "mafalda"))
+                      .build()
+                      .apply(orgMovie);
+
+                    return orgMovie.toString();
+                })
+                .thenAccept(response::resume);
 	}
-        
-        private String createMovieWithActor(Movie movie){
-            
-            //To json
-            String result = JsonbBuilder.create().toJson(movie);
-             
-            JsonReader jsonReader = Json.createReader(new StringReader(result));
-            JsonObject jobj = jsonReader.readObject();       
-            
-            //Json-p Patch
-            jobj = Json.createPatchBuilder()
-	      .add("/actores", "mafalda")
-	      .build()
-	      .apply(jobj);
-            
-            return jobj.toString();
-        }
 
 }
